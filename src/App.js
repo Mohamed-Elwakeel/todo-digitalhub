cf3d26b4d2c906fd9e18f55b08ccdd109cfc458d
mo7mdhassan7@gmail.com <mo7mdhassan7@gmail.com>
2024-03-11 11:44:19 +0200

    initialized app with reset styles and the initial styles for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Todo app</div>;
+  return (
+    <div className="App">
+      <div className="App">
+        <div className="todoCard">
+          <div className="App-header">
+            <header>Todo List</header>
+          </div>
+        </div>
+        <div className="appInfo">
+          <div>Created with: React JS, Material UI, CSS3, & Redux toolkit</div>
+          <div>Coded by: Mohamed Elwakeel</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
