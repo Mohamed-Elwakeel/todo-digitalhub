6832d6d260ca1ead94d1bf5897b746c554d5cd75
mo7mdhassan7@gmail.com <mo7mdhassan7@gmail.com>
2024-03-12 10:53:59 +0200

    AddTask component with full functionality and styles

--- src/Components/AddTask/AddTask.jsx
+++ src/Components/AddTask/AddTask.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AddTask.module.css";
 
-export const AddTask = () => {
+export const AddTask = ({ onSubmit }) => {
+    const [newTodo, setNewTodo] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (newTodo === "") return
+
+        onSubmit(newTodo);
+        setNewTodo("")
+    }
+
     return (
-        <form className={styles.addTaskContainer}>
-            <div>
-                <label htmlFor="new-item">Add new task</label>
-                <input
-                    className={styles.taskInput}
-                    type="text"
-                    id="new-item"
-                    placeholder="What needs to be done?"
-                ></input>
-            </div>
+        <form onSubmit={handleSubmit} className={styles.addTaskContainer}>
+            <label className={styles.taskInputLabel} htmlFor="new-todo">
+                Add new task
+            </label>
+            <input
+                className={styles.taskInput}
+                type="text"
+                id="new-todo"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="What needs to be done?"
+            ></input>
             <button className={styles.addTaskBtn}>Add</button>
         </form>
     );
